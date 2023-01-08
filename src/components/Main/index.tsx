14adb2454968e73266418e33ddf06339598f23d2
lilianleandro <lilianleeandro@gmail.com>
2023-01-08 20:07:23 -0300

    meu-site

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,20 +1,110 @@
 import * as S from '../styles'
 
 const Main = ({
-  title = 'Projeto da Lili',
-  description = 'TypeScript, ReactJS, NextJS e Styled Components'
+  title = 'Lilian Leandro',
+  description = 'Estudante de análise e desenvolvimento de sistemas, programadora front-end, viciada em café, apreciadora de literatura, astronomia e jogos antigos'
 }) => (
   <S.Wrapper>
-    <S.Logo
-      src="/img/logo.svg"
-      alt="Imagem de um átomo e React Avançado escrito ao lado."
-    />
-    <S.Title>{title}</S.Title>
+    <S.TitleContent>
+      <S.Logo
+        src="/img/flower-pot.png"
+        alt="Imagem de um átomo e React Avançado escrito ao lado."
+      />
+      <S.Title>{title}</S.Title>
+    </S.TitleContent>
     <S.Description>{description}</S.Description>
-    <S.Illustration
-      src="/img/hero-illustration.svg"
-      alt="Um desenvolvedor de frente para uma tela com código."
-    />
+    <S.TitleSection>Dados Profissionais</S.TitleSection>
+    <S.Section>
+      <S.SectionContent>
+        <b>Cargo</b>: Estágio em desenvolvimento web <br />
+        <b>Empresa</b>: MadeiraMadeira <br />
+        <b>Período</b>: 04/2021 à 04/2022
+      </S.SectionContent>
+      <S.SectionContent>
+        <b>Empresa</b>: MadeiraMadeira <br />
+        <b>Cargo</b>: Desenvolvedora frontend <br />
+        <b>Período</b>: Desde 04/2022
+      </S.SectionContent>
+    </S.Section>
+    <S.TitleSection>Formação Acadêmica</S.TitleSection>
+    <S.Section>
+      <S.SectionContent>
+        <b>Curso</b>: Tecnólogo em Análise e Desenvolvimento de Sistemas -
+        Uninter (2020 - 2023) <br />
+        <b>Situação</b>: Cursando
+      </S.SectionContent>
+    </S.Section>
+    <S.TitleSection>Formação Coplementar</S.TitleSection>
+    <S.Section>
+      <S.SectionContent>
+        <b>Curso</b>: Curso Web Moderno - Javascript
+        <br />
+        <b>Duração</b>: 97 horas
+        <br />
+        <b>Situação</b>: Completo
+        <br />
+      </S.SectionContent>
+      <S.SectionContent>
+        <b>Curso</b>:NextJS, GraphQL/GraphCMS e Leaflet na prática! -{' '}
+        <a href="https://my-locals.vercel.app/" title="meu projeto">
+          Link do projeto!
+        </a>
+        <br />
+        <b>Duração</b>: 10 horas
+        <br />
+        <b>Situação</b>: Completo <br />
+      </S.SectionContent>
+      <S.SectionContent>
+        <b>Curso</b>: React Avançado <br />
+        <b>Duração</b>: 88 horas
+        <br />
+        <b>Situação</b>: Cursando
+        <br />
+      </S.SectionContent>
+    </S.Section>
+    <S.TitleSection>Mídias Sociais</S.TitleSection>
+    <S.MediaSocial>
+      <S.MediaLink
+        href="https://www.instagram.com/lilianleandro95/"
+        title="instagram"
+        target="_blank"
+      >
+        {' '}
+        <S.Logo
+          src="/img/instagram (1).png"
+          alt="Imagem de um átomo e React Avançado escrito ao lado."
+        />
+      </S.MediaLink>
+      <S.MediaLink
+        href="https://www.linkedin.com/in/lilian-leandro/"
+        title="linkedin"
+        target="_blank"
+      >
+        <S.Logo
+          src="/img/linkedin (1).png"
+          alt="Imagem de um átomo e React Avançado escrito ao lado."
+        />
+      </S.MediaLink>
+    </S.MediaSocial>
+
+    <S.ContactForm
+      action="https://formsubmit.co/lilianleeandro@gmail.com"
+      method="post"
+    >
+      <S.FiledsetForm>
+        <S.TitleSection>Entre em contato</S.TitleSection>
+        <S.LabelForm htmlFor="nome">Nome</S.LabelForm>
+        <S.InputForm type="text" name="nome" />
+
+        <S.LabelForm htmlFor="nome">Email</S.LabelForm>
+        <S.InputForm type="text" name="email" />
+
+        <S.LabelForm htmlFor="nome">Mensagem</S.LabelForm>
+        <S.InputForm name="msg"></S.InputForm>
+
+        <S.InputButton type="submit" value="Pronto! ;)" />
+      </S.FiledsetForm>
+    </S.ContactForm>
   </S.Wrapper>
 )
 
--- src/components/styles.ts
+++ src/components/styles.ts
@@ -1,11 +1,10 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.main`
-  background-color: #06092b;
-  color: #fff;
-  width: 100%;
-  height: 100%;
+  background-color: #bd97ff;
+  color: #61615e;
   padding: 3rem;
+  width: 100%;
   text-align: center;
   display: flex;
   flex-direction: column;
@@ -13,18 +12,108 @@ export const Wrapper = styled.main`
   justify-content: center;
 `
 
+export const TitleContent = styled.div`
+  display: inline-flex;
+`
+
 export const Logo = styled.img`
-  width: 25rem;
+  width: 60px;
   margin-bottom: 2rem;
 `
 
 export const Title = styled.h1`
-  font-size: 2.5rem;
+  align-self: center;
+  font-size: 3.5rem;
 `
 
 export const Description = styled.h2`
   font-size: 2rem;
   font-weight: 400;
+  line-height: 3rem;
+  border: 2px solid #d9d8d2;
+  padding: 16px;
+  margin-bottom: 16px;
+  width: 100%;
+  max-width: 350px;
+  @media (min-width: 768px) {
+    width: 800px;
+  }
+`
+export const MediaLink = styled.a`
+  margin: 8px;
+`
+export const TitleSection = styled.h2`
+  font-size: 24px;
+  font-weight: 400;
+`
+export const Section = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`
+
+export const SectionContent = styled.section`
+  font-size: 16px;
+  text-align: left;
+
+  font-weight: 300;
+  background-color: #ccc;
+  line-height: 28px;
+  padding: 16px;
+  margin: 16px;
+  width: 350px;
+`
+
+export const ContactForm = styled.form`
+  display: flex;
+`
+export const LabelForm = styled.label`
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 28px;
+  align-self: center;
+  padding: 8px;
+`
+export const InputForm = styled.input`
+  border: #669 2px solid;
+  font-size: 12px;
+  display: flex;
+  align-self: center;
+  width: 480px;
+  border-radius: 4px;
+  padding: 8px;
+  @media (max-width: 768px) {
+    width: 280px;
+  }
+`
+
+export const FiledsetForm = styled.fieldset`
+  border: #ccc 1px dotted;
+  margin-top: 1em;
+  padding: 8px;
+  display: flex;
+  flex-direction: column;
+  @media (max-width: 768px) {
+    width: 280px;
+  }
+`
+
+export const InputButton = styled.input`
+  border: #bbb 1px solid;
+  border-radius: 4px;
+  font-size: 16px;
+  align-self: center;
+  line-height: 28px;
+  padding: 8px;
+  width: 80px;
+  margin-top: 0.5em;
+`
+
+export const MediaSocial = styled.div`
+  display: flex;
 `
 
 export const Illustration = styled.img`
